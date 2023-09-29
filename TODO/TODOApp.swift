b755986f9b27a27df5ab47934bebb59812a69669
Memo Diaz <121352998+memodiaz44@users.noreply.github.com>
2023-09-29 17:25:15 -0600

    Initial Commit

---
+++ TODO/TODOApp.swift
@@ -0,0 +1,17 @@
+//
+//  TODOApp.swift
+//  TODO
+//
+//  Created by Adel Diaz on 29/09/23.
+//
+
+import SwiftUI
+
+@main
+struct TODOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
