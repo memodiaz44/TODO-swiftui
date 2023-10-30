85c74924e8abf8c1950cd9aada676d2475965893
Memo Diaz <121352998+memodiaz44@users.noreply.github.com>
2023-10-30 04:54:03 -0600

    first commit

---
+++ TODO/Models/RegisterModel.swift
@@ -0,0 +1,24 @@
+//
+//  RegisterModel.swift
+//  TODO
+//
+//  Created by Adel Diaz on 02/10/23.
+//
+
+import Foundation
+
+class RegisterModel: ObservableObject {
+    @Published var name = ""
+    @Published var emall = ""
+    @Published var passwplrd = ""
+    
+    init(){}
+    
+    func register(){
+        
+    }
+    
+   private  func validate(){
+        
+    }
+}
